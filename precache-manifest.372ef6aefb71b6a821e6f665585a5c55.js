dc4336d5a131c4be1a25b9fdf4b123bc491a5566
Minh Phượng Đoàn <doanminhphuong.208@tcu.edu.vn>
2020-04-12 19:22:27 +0700

    Updates

---
+++ precache-manifest.372ef6aefb71b6a821e6f665585a5c55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73e2b8f76e42270dbee4f90970dd7202",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1b5eac74b5e882b49851",
+    "url": "/monsters-rolodex/static/css/main.0ab73cb7.chunk.css"
+  },
+  {
+    "revision": "b574afcd21eb60d6f292",
+    "url": "/monsters-rolodex/static/js/2.b72ce829.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b72ce829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b5eac74b5e882b49851",
+    "url": "/monsters-rolodex/static/js/main.763a0ffb.chunk.js"
+  },
+  {
+    "revision": "7c827a4688774389d883",
+    "url": "/monsters-rolodex/static/js/runtime-main.11adf8e6.js"
+  }
+]);
